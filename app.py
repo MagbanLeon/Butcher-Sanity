2703ffb2788f224c0fba3d4fc11804390af097ee
Leon Magbanua <112917169+MagbanLeon@users.noreply.github.com>
2025-12-11 23:51:53 +0000

    navbar coming together

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-#idk
-from flask import Flask, render_template, url_for, redirect
+# Leon Magbanua
+# HCI Project: Butcher Sanity
+# CS 420 - Human Computer Interaction
+
+#app.py
+#Primary application management performed here
+#Routes are listed, and most non-static, interactivity is coded here
+
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
+#Routes for interactive pages
 @app.route("/", methods = ['GET', 'POST'])
-def hello_world():
+def homepage(): #The homepage
     return render_template('homepage.html')
 
-app.run(debug=True, port=8090)
+@app.route("/searchPage")
+def search_page():  #Search function
+    return render_template('homepage.html')
+
+@app.route("/loginPage")
+def login_page():   #Potential login
+    print("bro")
+    return render_template('loginPage.html')
+
+@app.route("/meatCatalog")
+def meat_catalog():
+    return render_template('homepage.html')
+
+@app.route("/meatCarousel")
+def meat_carousel():
+    return render_template('homepage.html')
+
+@app.route("/shoppingCart")
+def chopping_cart():
+    return render_template('homepage.html')
+
+#Routes for information pages
+
+
+
+app.run(debug=True, port=8080)
